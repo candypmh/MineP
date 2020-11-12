ff481625c3e527e70c442c0c5f71bdafce025a83
Minep <pmh@ge.kornu.ac.kr>
2020-11-12 21:13:56 +0900

    ~2020.11.11

--- Study/src/대소문자변환/Test.java
+++ Study/src/대소문자변환/Test.java
@@ -6,19 +6,19 @@ public class Test {
 		
 		//----------------------------------------소문자변환
 		
-		String String1 = "MBZ1001_RCVORDER_EMP_ID";
-		String String2 = "MBZ1001_MGT_MEMP_ID";
+		String String1 = "MBZ1001_BUSINESS_OUTLINE_ADDR1"; //신청
+		String String2 = "MBZ_LAND_ADDR_ALL";//검토
 		String String3 = "MBZ1001_MGT_EMP_ID";
-		String String4 = "MBZ_OBTAIN_EMP_ID";
-		String String5 = "MBZ_MGT_EMP1_ID";
-		String String6 = "MBZ_MGT_EMP2_ID";
+//		String String4 = "MBZ_OBTAIN_EMP_ID";
+//		String String5 = "MBZ_MGT_EMP1_ID";
+//		String String6 = "MBZ_MGT_EMP2_ID";
 		
 		System.out.println(String1.toLowerCase());
 		System.out.println(String2.toLowerCase());
 		System.out.println(String3.toLowerCase());
-		System.out.println(String4.toLowerCase());
-		System.out.println(String5.toLowerCase());
-		System.out.println(String6.toLowerCase());
+//		System.out.println(String4.toLowerCase());
+//		System.out.println(String5.toLowerCase());
+//		System.out.println(String6.toLowerCase());
 
 		
 		
